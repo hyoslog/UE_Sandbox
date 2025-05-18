3fca466efc693672084b8f9ede3e2f1dfb465e70
hyoslog <hyoslog@gmail.com>
2025-05-18 23:56:42 +0900

    GAS 플러그인 활성화 및 Build.cs 수정

--- Source/SandBox/SandBox.Build.cs
+++ Source/SandBox/SandBox.Build.cs
@@ -9,5 +9,6 @@ public class SandBox : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+        PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+    }
 }
